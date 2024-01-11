b6e9c0d6acea87eddf2353e08f7e52d3c78f4eb8
Ibrahim Raimi <raimiibrahim44@gmail.com>
2024-01-11 23:44:02 +0100

    feat(data): add new dataset

---
+++ src/data/presentation.ts
@@ -0,0 +1,28 @@
+import type { Presentation } from "@/utils/types";
+
+const presentation: Presentation = {
+  mail: "contact@ibrahimraimi.com",
+  title: "Hey there 👋, I'm Ibrahim",
+  description:
+    "A software developer and occasional writer. As the mind behind Craftform, I'm committed to refining businesses through a dynamic approach to insightful web forms. When I'm not immersed in perfecting Craftform, I contribute and moderate content on *Dev.to*",
+  socials: [
+    {
+      label: "Twitter",
+      link: "https://twitter.com/ibrahimraimi_",
+    },
+    {
+      label: "GitHub",
+      link: "https://github.com/ibrahimraimi",
+    },
+    {
+      label: "LinkedIn",
+      link: "https://linkedin.com/in/ibrahimraimi",
+    },
+    {
+      label: "YouTube",
+      link: "https://youtube.com/@ibrahimraimi_",
+    },
+  ],
+};
+
+export default presentation;
---
+++ src/data/projects.ts
@@ -0,0 +1,26 @@
+import type { Project } from "@/utils/types";
+
+const projects: Project[] = [
+  {
+    title: "Craftform",
+    description:
+      "Craftform — the innovative form builder that redefines your business. Bid adieu to dull forms and welcome a dynamic approach to glean valuable insights.",
+    link: "",
+    isComingSoon: true,
+  },
+  {
+    title: "Astra Ecommerce",
+    description:
+      "A feature-rich eCommerce BaaS with dashboard, authentication, API, and advanced analytics.",
+    link: "",
+    isComingSoon: true,
+  },
+  {
+    title: "Devrider",
+    description: "A curated list of resources for frontend development.",
+    link: "",
+    isComingSoon: true,
+  },
+];
+
+export default projects;
